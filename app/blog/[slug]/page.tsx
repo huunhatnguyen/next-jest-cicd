b712225c789b0bb922c9db67c58e24ef6bf1fd39
Nhat Nguyen <huunhatnguyen92@gmail.com>
2024-08-31 23:03:27 +0700

    Add comment in page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,8 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>Ken</p></>;
+  return <><h1>Slug: {params.slug}</h1>
+  <p>Ken</p>
+  <p>Wellcome to HCM</p>
+  </>;
 }
